594a806ab98768fd8edac7a6eb8cb2a27ca13f90
Carlosjw <carlos.ti.infor@gmail.com>
2021-11-13 23:06:27 -0300

    SecondCommit

---
+++ MeuApp/Program.cs
@@ -0,0 +1,13 @@
+using System; // importação
+
+namespace MeuApp // divisões lógicas
+{
+    class Program // programa em si
+    {        
+        static void Main(string[] args)
+        {
+            var texto = "Testando";
+            Console.WriteLine(texto);
+        }   
+    }
+}
